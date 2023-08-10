91e6ad625a18f3d25c8a29ef3d165b367f28e421
santi <santi.arroyo551@gmail.com>
2023-08-10 08:56:45 -0700

    adds form to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,20 @@ function Contact() {
   return (
     <div className='content'>
       <h1>Contact</h1>
+         <form>
+            <div className='inputContainer'>
+                <label for='name'>Name</label>
+                <input type='text' name='name' id='name'/>
+            </div>
+            <div className='inputContainer'>
+              <label for='email'>Email</label>
+              <input type='email' name='email' id='email'/>
+            </div>
+            <div className='inputContainer'>
+              <label for='message'>Message</label>
+              <textarea name='message' id='message' rows='5' cols='30'/>
+            </div>
+         </form>
     </div>
   )
 }
